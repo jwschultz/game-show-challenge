abff1cd352b5c73dd339e431f322800cfe0323c8
jwschultz <jwschultz@gmail.com>
2015-03-16 21:15:57 -0400

    - Create project - Add elements to interface and try feebly to constrain them to look nice

--- Game Show Challenge/ViewController.h
+++ Game Show Challenge/ViewController.h
@@ -10,6 +10,33 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *playerScore;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+@property (weak, nonatomic) IBOutlet UIButton *button9;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button0;
+@property (weak, nonatomic) IBOutlet UIButton *buttonCancel;
+@property (weak, nonatomic) IBOutlet UIButton *dailyDouble;
+@property (weak, nonatomic) IBOutlet UIButton *finalWager;
+@property (weak, nonatomic) IBOutlet UIButton *correct;
+@property (weak, nonatomic) IBOutlet UIButton *incorrect;
+@property (weak, nonatomic) IBOutlet UIButton *singleValue200;
+@property (weak, nonatomic) IBOutlet UIButton *singleValue400;
+@property (weak, nonatomic) IBOutlet UIButton *singleValue600;
+@property (weak, nonatomic) IBOutlet UIButton *singleValue800;
+@property (weak, nonatomic) IBOutlet UIButton *singleValue1000;
+@property (weak, nonatomic) IBOutlet UIButton *doubleValue400;
+@property (weak, nonatomic) IBOutlet UIButton *doubleValue800;
+@property (weak, nonatomic) IBOutlet UIButton *doubleValue1200;
+@property (weak, nonatomic) IBOutlet UIButton *doubleValue1600;
+@property (weak, nonatomic) IBOutlet UIButton *doubleValue2000;
+
 
 @end
 
