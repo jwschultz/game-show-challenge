e2ad596073d5856fe7653b420d48f55ab043888b
jwschultz <jwschultz@gmail.com>
2015-03-28 14:59:07 -0400

    Air Dates - add airDate property to stuff - add a view before starting a new game to select the air date

---
+++ Game Show Challenge/NewGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewGameViewController.h
+//  Game Show Challenge
+//
+//  Created by John Schultz on 3/28/15.
+//  Copyright (c) 2015 Friends of Hektor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface NewGameViewController : ViewController
+
+@end
---
+++ Game Show Challenge/NewGameViewController.m
@@ -0,0 +1,50 @@
+//
+//  NewGameViewController.m
+//  Game Show Challenge
+//
+//  Created by John Schultz on 3/28/15.
+//  Copyright (c) 2015 Friends of Hektor. All rights reserved.
+//
+
+#import "NewGameViewController.h"
+#import "GameShowGame.h"
+
+@interface NewGameViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *airDate;
+
+@end
+
+const static NSInteger TODAYS_GAME_TAG = 0;
+const static NSInteger ANOTHER_AIRDATE_TAG = 1;
+
+@implementation NewGameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.airDate.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    ViewController *viewController = [segue destinationViewController];
+    UIButton *senderButton = (UIButton*)sender;
+    
+    viewController.jeopardyGame = [[GameShowGame alloc] init];
+    switch (senderButton.tag) {
+        case TODAYS_GAME_TAG:
+            // just use the default date from the GameShowGame init method
+            break;
+        case ANOTHER_AIRDATE_TAG:
+            viewController.jeopardyGame.airDate = self.airDate.date;
+        default:
+            break;
+    }
+}
+
+@end
