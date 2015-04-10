531aec53718cc1a8291287e38e4f57cb170bad1a
John Schultz <johnwsch@med.umich.edu>
2015-04-09 23:18:54 -0400

    Add twitter user, barely

---
+++ Game Show Challenge/UserAccountSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserAccountSelectionViewController.h
+//  Game Show Challenge
+//
+//  Created by John Schultz on 4/9/15.
+//  Copyright (c) 2015 Friends of Hektor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserAccountSelectionViewController : UIViewController
+
+@end
---
+++ Game Show Challenge/UserAccountSelectionViewController.m
@@ -0,0 +1,49 @@
+//
+//  UserAccountSelectionViewController.m
+//  Game Show Challenge
+//
+//  Created by John Schultz on 4/9/15.
+//  Copyright (c) 2015 Friends of Hektor. All rights reserved.
+//
+
+#import "UserAccountSelectionViewController.h"
+#import <Parse/Parse.h>
+
+@interface UserAccountSelectionViewController ()
+
+@end
+
+@implementation UserAccountSelectionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)signInToService:(UIButton *)sender {
+    PFUser *user = [PFUser currentUser];
+    
+    if (![PFTwitterUtils isLinkedWithUser:user]) {
+        [PFTwitterUtils linkUser:user block:^(BOOL succeeded, NSError *error) {
+            if ([PFTwitterUtils isLinkedWithUser:user]) {
+                NSLog(@"Woohoo, user logged in with Twitter!");
+            }
+        }];
+    }
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
